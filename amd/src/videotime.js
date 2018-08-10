b318b60fab194d969fa2ad2f14e4f90a2196f6b5
moodledev <moodledev@gmail.com>
2018-08-09 19:21:03 -0500

    JavaScript updates

--- amd/src/videotime.js
+++ amd/src/videotime.js
@@ -7,9 +7,9 @@
 /**
  * @module block_overview/helloworld
  */
-define(['jquery', 'mod_videotime/player'], function($, Vimeo) {
+define(['jquery', 'mod_videotime/player', 'core/ajax'], function($, Vimeo, Ajax) {
     return {
-        init: function() {
+        init: function(moduleId, userId) {
             console.log("INIT");
             var player = new Vimeo('vimeo-embed', {
                 responsive: 1
@@ -25,6 +25,13 @@ define(['jquery', 'mod_videotime/player'], function($, Vimeo) {
             player.on('timeupdate', function() {
                 console.log('timeupdate.');
             });
+
+            setInterval(function() {
+                Ajax.call([{
+                    methodname: 'mod_videotime_record_watch_time',
+                    args: { user_id: userId, module_id: moduleId }
+                }]);
+            }, 5000);
         }
     };
 });
