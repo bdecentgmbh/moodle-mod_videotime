bcf4beb5f0a7055a873447e3e950613545aaf5f6
moodledev <moodledev@gmail.com>
2018-08-09 19:20:35 -0500

    Add event

--- index.php
+++ index.php
@@ -34,7 +34,7 @@ require_course_login($course);
 $coursecontext = context_course::instance($course->id);
 
 $event = \mod_videotime\event\course_module_instance_list_viewed::create(array(
-    'context' => $modulecontext
+    'context' => $coursecontext
 ));
 $event->add_record_snapshot('course', $course);
 $event->trigger();
