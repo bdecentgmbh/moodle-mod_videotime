59f2c4681cd2d2b8c10e59fe4f25d6f974d4ed27
moodledev <moodledev@gmail.com>
2018-09-05 16:25:42 -0500

    Fix conditional

--- lang/en/videotime.php
+++ lang/en/videotime.php
@@ -39,7 +39,7 @@ $string['embed_options'] = 'Embed options';
 $string['embed_options_defaults'] = 'Default embed options';
 $string['modulename'] = 'Video Time';
 $string['modulenameplural'] = 'Video Time instances';
-if (videotime_has_pro()) {
+if (!videotime_has_pro()) {
     $string['modulename_help'] = 'The Video Time activity enables the teacher
 <ul>
     <li>to easily embed videos from Vimeo, just by adding the url</li>
