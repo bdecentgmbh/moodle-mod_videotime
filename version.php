b7bbd41da52a943f504188bfbc055b9fa16b5279
moodledev <moodledev@gmail.com>
2019-09-16 22:12:23 -0500

    Next button compatible with bootstrap 3

--- version.php
+++ version.php
@@ -26,6 +26,6 @@ defined('MOODLE_INTERNAL') || die();
 
 $plugin->component = 'mod_videotime';
 $plugin->release = '1.1';
-$plugin->version = 2019091401;
+$plugin->version = 2019091601;
 $plugin->requires = 2015111610;
 $plugin->maturity = MATURITY_STABLE;
