110f0d7a2206405e91357d8a449f7dce459a53b5
moodledev <moodledev@gmail.com>
2020-03-31 13:25:10 -0500

    Rebuild JS

--- version.php
+++ version.php
@@ -26,6 +26,6 @@ defined('MOODLE_INTERNAL') || die();
 
 $plugin->component = 'mod_videotime';
 $plugin->release = '1.2.1';
-$plugin->version = 2020032601;
+$plugin->version = 2020033100;
 $plugin->requires = 2015111610;
 $plugin->maturity = MATURITY_STABLE;
