dff5f6a3a52b478029c55b2fef7b027112db40dd
moodledev <pa.nhia.lee@nyiaj.io>
2020-07-22 16:19:26 -0500

    CU-6rkntt Fix Video Time intro encoding

--- classes/videotime_instance.php
+++ classes/videotime_instance.php
@@ -249,8 +249,6 @@ class videotime_instance implements \renderable, \templatable {
         $record->video_description = file_rewrite_pluginfile_urls($record->video_description, 'pluginfile.php',
             $this->get_context()->id, 'mod_videotime', 'video_description', 0);
 
-        $record->intro = htmlspecialchars($record->intro, ENT_QUOTES); // Encode quotes for preview mode description modal.
-
         $record->intro_excerpt = videotime_get_excerpt($record->intro);
         $record->show_more_link = strlen(strip_tags($record->intro_excerpt)) < strlen(strip_tags($record->intro));
 
