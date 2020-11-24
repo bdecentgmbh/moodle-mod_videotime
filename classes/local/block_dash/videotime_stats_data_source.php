276520c51a8c9f57d5238c4b34c777689fbb1fd7
moodledev <pa.nhia.lee@nyiaj.io>
2020-11-24 09:44:53 -0600

    DASH-173 add filters and fields to video time stats data source

--- classes/local/block_dash/videotime_stats_data_source.php
+++ classes/local/block_dash/videotime_stats_data_source.php
@@ -23,12 +23,20 @@
 namespace mod_videotime\local\block_dash;
 
 use block_dash\local\dash_framework\query_builder\builder;
+use block_dash\local\dash_framework\query_builder\join;
+use block_dash\local\data_grid\filter\bool_filter;
+use block_dash\local\data_grid\filter\course_condition;
+use block_dash\local\data_grid\filter\current_course_condition;
+use block_dash\local\data_grid\filter\date_filter;
 use block_dash\local\data_grid\filter\filter_collection;
 use block_dash\local\data_grid\filter\filter_collection_interface;
 use block_dash\local\data_source\abstract_data_source;
 use context;
-use local_dash\data_grid\filter\my_dashboards_condition;
-use local_dash\data_grid\filter\nonpublic_dashboards_condition;
+use local_dash\data_grid\filter\category_field_filter;
+use local_dash\data_grid\filter\course_category_condition;
+use local_dash\data_grid\filter\course_field_filter;
+use local_dash\data_grid\filter\customfield_filter;
+use local_dash\data_grid\filter\my_enrolled_courses_condition;
 use local_dash\local\dash_framework\structure\course_table;
 use mod_videotime\local\dash_framework\structure\videotime_table;
 
@@ -53,7 +61,33 @@ class videotime_stats_data_source extends abstract_data_source {
         $builder
             ->select('vt.id', 'vt_id')
             ->from('videotime', 'vt')
-            ->join('course', 'c', 'id', 'vt.course');
+            ->join('course', 'c', 'id', 'vt.course')
+            ->join('course_categories', 'cc', 'id', 'c.category');
+
+        $filterpreferences = $this->get_preferences('filters');
+
+        if (class_exists('\core_course\customfield\course_handler')) {
+            $handler = \core_course\customfield\course_handler::create();
+            foreach ($handler->get_fields() as $field) {
+                $alias = 'c_f_' . strtolower($field->get('shortname'));
+                // Only join custom field table if the filter is enabled.
+                if (isset($filterpreferences[$alias]) && $filterpreferences[$alias]['enabled']) {
+                    $builder->join('customfield_data', $alias, 'instanceid', 'c.id', join::TYPE_LEFT_JOIN)
+                        ->join_condition($alias, "$alias.fieldid = " . $field->get('id'));
+                }
+            }
+        } else if (block_dash_is_totara()) {
+            global $DB;
+
+            foreach ($DB->get_records('course_info_field') as $field) {
+                $alias = 'c_f_' . strtolower($field->shortname);
+                // Only join custom field table if the filter is enabled.
+                if (isset($filterpreferences[$alias]) && $filterpreferences[$alias]['enabled']) {
+                    $builder->join('course_info_data', $alias, 'courseid', 'c.id', join::TYPE_LEFT_JOIN)
+                        ->join_condition($alias, "$alias.fieldid = " . $field->get('id'));
+                }
+            }
+        }
 
         return $builder;
     }
@@ -65,9 +99,66 @@ class videotime_stats_data_source extends abstract_data_source {
     {
         $filter_collection = new filter_collection(get_class($this), $this->get_context());
 
-        $filter_collection->add_filter(new my_dashboards_condition('dd_id', 'dd.id'));
+        $filter_collection->add_filter(new category_field_filter('cc', 'cc.id', get_string('category')));
 
-        $filter_collection->add_filter(new nonpublic_dashboards_condition('dd_nonpublic', 'dd.id'));
+        $filter_collection->add_filter(new course_field_filter('c', 'c.id', get_string('course')));
+
+        if (class_exists('\core_course\customfield\course_handler')) {
+            $handler = \core_course\customfield\course_handler::create();
+            foreach ($handler->get_fields() as $field) {
+
+                $alias = 'c_f_' . strtolower($field->get('shortname'));
+                $select = $alias . '.value';
+
+                switch ($field->get('type')) {
+                    case 'checkbox':
+                        $definitions[] = new bool_filter($alias, $select, $field->get_formatted_name());
+                        break;
+                    case 'date':
+                        $filter_collection->add_filter(new date_filter($alias, $select, date_filter::DATE_FUNCTION_FLOOR,
+                            $field->get_formatted_name()));
+                        break;
+                    case 'textarea':
+                        break;
+                    default:
+                        $filter_collection->add_filter(new customfield_filter($alias, $select, $field,
+                            $field->get_formatted_name()));
+                        break;
+                }
+            }
+        } else if (block_dash_is_totara()) {
+            global $DB;
+
+            foreach ($DB->get_records('course_info_field') as $field) {
+
+                $alias = 'c_f_' . strtolower($field->shortname);
+                $select = $alias . '.data';
+
+                switch ($field->datatype) {
+                    case 'checkbox':
+                        $definitions[] = new bool_filter($alias, $select, $field->fullname);
+                        break;
+                    case 'date':
+                        $filter_collection->add_filter(new date_filter($alias, $select, date_filter::DATE_FUNCTION_FLOOR,
+                            $field->fullname));
+                        break;
+                    case 'textarea':
+                        break;
+                    default:
+                        $filter_collection->add_filter(new customfield_filter($alias, $select, $field,
+                            $field->fullname));
+                        break;
+                }
+            }
+        }
+
+        $filter_collection->add_filter(new current_course_condition('c_current_course', 'c.id'));
+
+        $filter_collection->add_filter(new course_condition('c_course', 'c.id'));
+
+        $filter_collection->add_filter(new my_enrolled_courses_condition('my_enrolled_courses', 'c.id'));
+
+        $filter_collection->add_filter(new course_category_condition('c_course_categories_condition', 'c.category'));
 
         return $filter_collection;
     }
