7c4fcf4d0a459a691be6c57d75a863cf3e23da46
Daniel Thies <dethies@gmail.com>
2021-11-30 20:33:16 -0600

    VID-300: Change text track name to transcript

--- tab/texttrack/lang/en/videotimetab_texttrack.php
+++ tab/texttrack/lang/en/videotimetab_texttrack.php
@@ -24,8 +24,8 @@
 
 defined('MOODLE_INTERNAL') || die();
 
-$string['pluginname'] = 'Video Time Text track tab';
-$string['label'] = 'Text track';
-$string['privacy:metadata'] = 'The Video Time Text track tab plugin does not store any personal data.';
+$string['pluginname'] = 'Video Time Transcript tab';
+$string['label'] = 'Transcript';
+$string['privacy:metadata'] = 'The Video Time Transcript tab plugin does not store any personal data.';
 $string['default'] = 'Default';
 $string['default_help'] = 'Whether tab is enabled by default';
