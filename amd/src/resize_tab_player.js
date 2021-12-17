b61b934017e567de8c92a49c66ec4ce2f626a720
Daniel Thies <dethies@gmail.com>
2021-12-17 13:13:49 -0600

    VID-297: Implement resizing tab divider in js

--- amd/src/resize_tab_player.js
+++ amd/src/resize_tab_player.js
@@ -1,42 +1,87 @@
 /*
+ * Position the Vimeo player within tab layout
+ *
  * @package    mod_videotime
+ * @module     mod_videotime/resize_tab_player
  * @copyright  2021 bdecent gmbh <https://bdecent.de>
  * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
  */
 
 /**
- * @module mod_videotime/resize_tab_player
+ * Intialize listener
  */
-define([
-    'jquery',
-], function(
-    $
-) {
-    let Resizer = function() {
-    };
+export const initialize = () => {
+    var observer = new ResizeObserver(resize);
+    document.querySelectorAll('.tab-pane .instance-container, div.videotime-tab-instance').forEach(function (container) {
+        observer.observe(container);
+    }.bind(this));
+    resize();
+    document.querySelectorAll('.videotime-tab-instance').forEach((instance) => {
+        instance.style.position = 'absolute';
+    });
 
-    Resizer.prototype.init = function() {};
-    Resizer.prototype.initialize = function() {
-        var observer = new ResizeObserver(this.resize);
-        $('.tab-pane .instance-container, div.videotime-tab-instance').each(function(index, container) {
-            observer.observe(container);
-        }.bind(this));
-        this.resize();
-    };
+    window.removeEventListener('mousemove', mousemoveHandler);
+    window.addEventListener('mousemove', mousemoveHandler);
 
-    Resizer.prototype.resize = function() {
-        $('.tab-pane.active .instance-container').each(function(index, container) {
-            $('.vimeo-embed iframe').attr('width', $(container));
-            $('.vimeo-embed iframe').each(function() {
-                this.width = container.offsetWidth - 10;
-                $(container).css('height', this.parentNode.offsetHeight + 20 + 'px');
-                $('.tab-pane').css('min-height', this.parentNode.parentNode.offsetHeight + 20 + 'px');
-                $(this).closest('.videotime-tab-instance').css('top', $(container).position().top + 'px');
-                $(this).closest('.videotime-tab-instance').css('left', $(container).position().left + 'px');
-                $(this).closest('.videotime-tab-instance').css('width', container.offsetWidth + 'px');
+    window.removeEventListener('dragstart', dragstartHandler);
+    window.addEventListener('dragstart', dragstartHandler);
+
+    window.removeEventListener('mouseup', dragendHandler);
+    window.addEventListener('mouseup', dragendHandler);
+};
+
+/**
+ * Adjust player position when the page configuration is changed
+ */
+const resize = () => {
+    document.querySelectorAll('.tab-pane.active .instance-container').forEach((container) => {
+        document.querySelectorAll('.vimeo-embed iframe').forEach((iframe) => {
+            iframe.width = container.offsetWidth - 10;
+            document.querySelectorAll('.tab-pane').forEach((pane) => {
+                pane.style.minHeight =  iframe.parentNode.parentNode.offsetHeight + 20 + 'px';
             });
+            Object.assign(iframe.closest('.videotime-tab-instance').style, {
+                top: container.offsetTop + 'px',
+                left: container.offsetLeft + 'px',
+                width: container.offsetWidth + 'px'
+            });
+            container.style.minHeight = iframe.closest('.videotime-tab-instance').offsetHeight + 'px';
         });
-    };
+    });
+};
 
-    return new Resizer();
-});
+/**
+ * Reset handle when drag ends
+ */
+const dragendHandler = () => {
+    document.querySelectorAll('.videotimetab-resize-handle').forEach((h) => {
+        h.style.position = 'relative';
+    });
+};
+
+/**
+ * Prepare to drag border
+ *
+ * @param {event} e mouse event
+ */
+const dragstartHandler = (e) => {
+    if (e.target.classList.contains('videotimetab-resize-handle')) {
+        e.stopPropagation();
+        e.preventDefault();
+        e.target.style.position = 'absolute';
+    }
+};
+
+/**
+ * Resize the content and player to mouse location
+ *
+ * @param {event} e mouse event
+ */
+const mousemoveHandler = (e) => {
+    document.querySelectorAll('.videotimetab-resize-handle').forEach((h) => {
+        if (h.style.position === 'absolute') {
+            let column = h.closest('.tab-pane').querySelector('.videotimetab-resize');
+            column.style.width = e.pageX - column.getBoundingClientRect().left + 'px';
+        }
+    });
+};
