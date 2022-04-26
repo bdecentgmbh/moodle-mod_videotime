34f1d2ca54a800569b540fa83c5ab2209c437fe2
Nithy1anandan <nithyanandan.m.2016@lmsace.com>
2022-04-26 16:05:04 +0530

    version updated

--- version.php
+++ version.php
@@ -26,6 +26,6 @@ defined('MOODLE_INTERNAL') || die();
 
 $plugin->component = 'mod_videotime';
 $plugin->release = '1.5.1';
-$plugin->version = 2022041100;/* Lmsace */
+$plugin->version = 2022042600;/* Lmsace */
 $plugin->requires = 2015111610;
 $plugin->maturity = MATURITY_STABLE;
