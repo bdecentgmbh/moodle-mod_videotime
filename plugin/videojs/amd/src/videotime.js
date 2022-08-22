2edac260040c81885664b4c33dff65521ca82b7e
Daniel Thies <dethies@gmail.com>
2022-08-22 11:24:39 -0500

    VID-589: Set fluid option instead of responsive

--- plugin/videojs/amd/src/videotime.js
+++ plugin/videojs/amd/src/videotime.js
@@ -24,7 +24,7 @@ export default class VideoTime extends VideoTimeBase {
                 autoplay: instance.autoplay,
                 controls: instance.controls,
                 loop: instance.option_loop,
-                responsive: instance.responsive,
+                fluid: instance.responsive,
                 playbackRates: instance.speed ? [0.5, 0.75, 1, 1.25, 1.5, 2] : [1],
                 muted: instance.muted
             };
