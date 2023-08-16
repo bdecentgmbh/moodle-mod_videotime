c1f58d15d00bb82f2a8b9ff7d8b572388dabef99
Daniel Thies <dthies@gmail.com>
2023-08-15 22:11:56 -0500

    VID-683: Fix js linting

--- version.php
+++ version.php
@@ -25,8 +25,8 @@
 defined('MOODLE_INTERNAL') || die();
 
 $plugin->component = 'mod_videotime';
-$plugin->release = '1.7.3';
-$plugin->version = 2023011207;
+$plugin->release = '1.8';
+$plugin->version = 2023011208;
 $plugin->incompatible = 402;
 $plugin->requires = 2015111610;
-$plugin->maturity = MATURITY_STABLE;
+$plugin->maturity = MATURITY_BETA;
