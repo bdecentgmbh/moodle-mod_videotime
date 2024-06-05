4ae7e98394749066f834fdbc7151b2bb3f29e930
Daniel Thies <dthies@gmail.com>
2024-06-05 11:24:02 -0500

    VID-811:Separate timing functions for session update

--- version.php
+++ version.php
@@ -25,8 +25,8 @@
 defined('MOODLE_INTERNAL') || die();
 
 $plugin->component = 'mod_videotime';
-$plugin->release = '1.8';
-$plugin->version = 2024050600;
+$plugin->release = '1.8.1';
+$plugin->version = 2024050601;
 $plugin->incompatible = 402;
 $plugin->requires = 2015111610;
 $plugin->maturity = MATURITY_STABLE;
