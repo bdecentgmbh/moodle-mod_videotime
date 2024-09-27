570d6bf77c256092e91aef4e36694f2e7f5ad4b6
Daniel Thies <dthies@gmail.com>
2024-09-27 13:49:09 -0500

    VID-857: Automate go to next activity with VideoJS

--- plugin/videojs/amd/src/videotime.js
+++ plugin/videojs/amd/src/videotime.js
@@ -237,7 +237,7 @@ export default class VideoTime extends VideoTimeBase {
 
         // Initiate video finish procedure.
         this.player.on("ended", this.handleEnd.bind(this));
-        this.player.on("pause", this.handleEnd.bind(this));
+        this.player.on("pause", this.handlePause.bind(this));
 
         // Readjust height when responsive player is resized.
         if (this.player.options().responsive) {
