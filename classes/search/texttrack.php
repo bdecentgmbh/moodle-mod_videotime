62aaa11ca53f8c15230532cd0437f453dd6102ce
Daniel Thies <dethies@gmail.com>
2024-11-14 08:44:40 -0600

    VID-907: Return recordset when disabled

--- classes/search/texttrack.php
+++ classes/search/texttrack.php
@@ -49,7 +49,7 @@ class texttrack extends \core_search\base_mod {
         global $DB;
 
         if (!get_config('videotimetab_texttrack', 'enabled')) {
-            return null;
+            return $DB->get_recordset_select('videotime', 'FALSE');
         }
 
         [$contextjoin, $contextparams] = $this->get_context_restriction_sql(
