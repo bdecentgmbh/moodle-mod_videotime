81e30937b9f7aa2d0440d5534b1ffe0b10b0260c
Daniel Thies <dthies@gmail.com>
2025-03-22 08:57:01 -0500

    VID-985: Upgrade Vimeo

--- version.php
+++ version.php
@@ -25,7 +25,7 @@
 defined('MOODLE_INTERNAL') || die();
 
 $plugin->component = 'mod_videotime';
-$plugin->release = '1.8.5';
-$plugin->version = 2024050605;
+$plugin->release = '1.9 RC';
+$plugin->version = 2025032000;
 $plugin->requires = 2023042400;
 $plugin->maturity = MATURITY_STABLE;
