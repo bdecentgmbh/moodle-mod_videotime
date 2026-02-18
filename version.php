67550c2e44402485fc404ad66b8e86e229356a75
Daniel Thies <dthies@gmail.com>
2026-02-18 12:02:48 -0600

    Update version for release 1.10.2

--- version.php
+++ version.php
@@ -25,8 +25,8 @@
 defined('MOODLE_INTERNAL') || die();
 
 $plugin->component = 'mod_videotime';
-$plugin->release = '1.10.1';
-$plugin->version = 2025102501;
+$plugin->release = '1.10.2';
+$plugin->version = 2025102502;
 $plugin->requires = 2024041600;
 $plugin->supported = [404, 501];
 $plugin->maturity = MATURITY_STABLE;
